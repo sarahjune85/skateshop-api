a4b612b73573f66271dccc4ad23b88b7f9b04466
Sarah <sarahjune@gmail.com>
2021-12-06 01:17:06 +1100

    product schema tweak

--- models/Product.js
+++ models/Product.js
@@ -21,15 +21,16 @@ const ProductSchema = new mongoose.Schema(
       type: Array,
     },
     size: {
-      type: String,
+      type: Array,
     },
     color: {
-      type: String,
+      type: Array,
     },
     price: {
       type: Number,
       required: true,
     },
+    inStock: { type: Boolean, default: true },
   },
   { timestamps: true }
 );
